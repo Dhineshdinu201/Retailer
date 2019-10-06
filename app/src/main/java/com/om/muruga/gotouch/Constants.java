5874ad8265ba2daa860ffdefcb020c982bf9c4b3
dhinesh <dhineshdinu201@gmail.com>
2019-10-06 20:49:58 +0530

    tiruppur

--- app/src/main/java/com/om/muruga/gotouch/Constants.java
+++ app/src/main/java/com/om/muruga/gotouch/Constants.java
@@ -4,7 +4,10 @@ public class Constants {
     private static final String IP = "103.91.84.218";
     //private static final String IP = "192.168.1.7";
     private static final String PORT= ":9006";
+    public static final Integer VERSION = 2;
 
     public static final String URL = "http://"+IP+PORT;
+    public static final String APP_VERSION = "APP_VERSION";
 
+    public static final String APP_URL="APP_URL";
 }
--- app/src/main/java/com/om/muruga/gotouch/MainActivity.java
+++ app/src/main/java/com/om/muruga/gotouch/MainActivity.java
@@ -1,6 +1,9 @@
 package com.om.muruga.gotouch;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,25 +15,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static com.om.muruga.gotouch.Constants.URL;
 
 public class MainActivity extends AppCompatActivity {
     ProgressDialog progress = null;
     EditText userName,password;
+    String GET_URL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         userName = (EditText)findViewById(R.id.editText3);
         password = (EditText)findViewById(R.id.editText4);
         progress = new ProgressDialog(this);
+        //showalert();
     }
 
     @Override
@@ -48,6 +74,84 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public void api() {
+        GET_URL =  URL+"/v1/gotouch/retailer/getSettings";
+        Log.i("url",GET_URL);
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest request = new StringRequest(Request.Method.GET, GET_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.i("My success", "" + response);
+
+
+                try {
+                    //will receive id when the register is success
+                    JSONArray js = new JSONArray(response);
+                    Type type = new TypeToken<List<Settings>>() {
+                    }.getType();
+                    Gson gson = new Gson();
+                    List<Settings> locList = gson.fromJson(response, type);
+                    boolean isUpdateAvail=false;
+                    String url = "";
+                    for(Settings setting : locList){
+                        if(setting.getName().equalsIgnoreCase(Constants.APP_VERSION)){
+                            if(Constants.VERSION.intValue()<(Integer.parseInt(setting.getValue()))){
+                                isUpdateAvail = true;
+                            }
+                        }else if(setting.getName().equalsIgnoreCase(Constants.APP_VERSION)){
+                            url = setting.getValue();
+                        }
+                    }
+
+                    if(isUpdateAvail){
+                        showalert(url);
+                    }
+
+                    //************parsing response object**********
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(MainActivity.this, "Please check connectivity", Toast.LENGTH_SHORT).show();
+                Log.i("My error", "" + error);
+            }
+        }) {
+            @Override
+
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<String, String>();
+                //send your params here
+
+                return map;
+            }
+        };
+        queue.add(request);
+
+    }
+    public void showalert(String url){
+        Activity activity = null;
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        Button close;
+
+        final AlertDialog alertDialog = dialogBuilder.create();
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View vi = factory.inflate(R.layout.alert_update, null);
+        String value = "<html><a href=\""+url+"\">Click here to install</a></html>";
+        TextView text = (TextView)vi.findViewById(R.id.text);
+        text.setText(Html.fromHtml(value));
+        text.setMovementMethod(LinkMovementMethod.getInstance());
+        alertDialog.setView(vi);
+        alertDialog.show();
+        alertDialog.setCancelable(false);
+
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -130,5 +234,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
+
 
 
